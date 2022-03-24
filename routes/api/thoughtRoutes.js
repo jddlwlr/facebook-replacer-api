946f858303ad4ddc93c5f2f4ea7e1640bb18241c
Jesse Lawler <jedd.lawler@gmail.com>
2022-03-24 08:48:38 -0600

    putting thought routes in correct file- oops

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -15,3 +15,35 @@
 // PUT to update a thought by its _id
 
 // DELETE to remove a thought by its _id
+
+const { Thought, User } = require("../../models");
+
+module.exports = {
+  // get thoughts - all
+  getAllThoughts(req, res) {
+    Thought.find()
+      .then((thoughts) => res.json(thoughts))
+      .catch((err) => res.status(500).json(err));
+  },
+  //get thoughts - by id
+  getOneThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .select("-__v")
+      .then((thought) =>
+        !thought
+          ? res
+              .status(404)
+              .json({ message: "No thought associated with provided ID" })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  createThought(req, res) {
+    Thought.create(req.body)
+      .then((thought) => res.json(thought))
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+};
